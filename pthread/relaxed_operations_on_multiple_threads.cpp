85851b7bbf232a9562ec663bfeaddd7d538f99da
wtsgit <wtsgit@github>
2014-04-03 11:36:54 -0400

    Relaxed operations on multiple threads.

---
+++ pthread/relaxed_operations_on_multiple_threads.cpp
@@ -0,0 +1,80 @@
+// Anthony Williams, "C++ Concurrency in Action: Practical Multithreading," Manning, 2012.
+//
+// Listing 5.6 Relaxed operations on multiple threads.
+
+#include <atomic>
+#include <thread>
+#include <iostream>
+
+std::atomic<int> x(0);
+std::atomic<int> y(0);
+std::atomic<int> z(0);
+
+std::atomic<bool> go(false);
+
+const int N = 10;
+
+struct read_values {
+	int x, y, z;
+};
+
+read_values values1[N];
+read_values values2[N];
+read_values values3[N];
+read_values values4[N];
+read_values values5[N];
+
+void increment(std::atomic<int> *var_to_inc, read_values *values) {
+	while (!go)
+		std::this_thread::yield();
+	for (int i = 0; i < N; ++i) {
+		values[i].x = x.load(std::memory_order_relaxed);
+		values[i].y = y.load(std::memory_order_relaxed);
+		values[i].z = z.load(std::memory_order_relaxed);
+		var_to_inc->store(i+1, std::memory_order_relaxed);
+		std::this_thread::yield();
+	}
+}
+
+void read_vals(read_values *values) {
+	while (!go)
+		std::this_thread::yield();
+	for (int i = 0; i < N; ++i) {
+		values[i].x = x.load(std::memory_order_relaxed);
+		values[i].y = y.load(std::memory_order_relaxed);
+		values[i].z = z.load(std::memory_order_relaxed);
+		std::this_thread::yield();
+	}
+}
+
+void print(read_values *v) {
+	for (int i = 0; i < N; ++i) {
+		if (i) std::cout << ", ";
+		std::cout << "(" << v[i].x << ", " << v[i].y << ", " << v[i].z << ")";
+	}
+	std::cout << std::endl;
+}
+
+int main() {
+	std::thread t1(increment, &x, values1);
+	std::thread t2(increment, &y, values2);
+	std::thread t3(increment, &z, values3);
+	std::thread t4(read_vals, values4);
+	std::thread t5(read_vals, values5);
+
+	go = true;
+
+	t5.join();
+	t4.join();
+	t3.join();
+	t2.join();
+	t1.join();
+
+	print(values1);
+	print(values2);
+	print(values3);
+	print(values4);
+	print(values5);
+
+	return 0;
+}
